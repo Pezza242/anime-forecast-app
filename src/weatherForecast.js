0230ef572dbefc4667e64f3362a8a49577c06cbb
Pezza242 <periyearwood@gmail.com>
2026-05-03 11:23:26 +0100

    Added weatherForecast.css
    Add CSS to the forecast section to make it tidier.

--- src/weatherForecast.js
+++ src/weatherForecast.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./weatherForecast.css";
 
 export default function WeatherForecast(props) {
   let [loading, setLoading] = useState(false);
   //let [forecast, setForcast] = useState(null);
 
-  let longitude = props.coords.lat;
-  let latitude = props.coords.lon;
-  let apiKey = "aa9f555583ebe9bb2cc64bd6b978ad42";
+  //let longitude = props.coords.lat;
+  //let latitude = props.coords.lon;
+  //let apiKey = "aa9f555583ebe9bb2cc64bd6b978ad42";
+
   function handleResponse(response) {
     setLoading(true);
-    console.log(response.data);
+    console.log(response.data.list);
   }
 
-  let apiUrl = `api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=0.1276&lon=51.5072&appid=aa9f555583ebe9bb2cc64bd6b978ad42`;
   axios.get(apiUrl).then(handleResponse);
 
   if (loading) {
